7f59ad74411a006f23c388bb02ef7a889af6c7b2
Joey Comhaire <joey.comhaire@gmail.com>
2022-11-15 22:53:29 +0100

    Added extra method layer to make the for loop shoreter

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -14,22 +14,9 @@ export class GildedRose {
 
   updateQuality() {
     for (let item of this.items) {
-      const doesDecrease = item.name !== AGED_BRIE && item.name !== BACKSTAGE_PASS && item.name !== SULFURAS;
       const hasSellByDate = item.name != SULFURAS;
-      const isExpired = item.sellIn < 1;
 
-      if (doesDecrease) {
-        const decreaseRate = this.getDecreaseRate(item);
-        const decrease = isExpired ? 2 * decreaseRate : decreaseRate;
-        this.updateItemQuality(item, decrease);
-      }
-      if (item.name === AGED_BRIE) {
-        const increase = isExpired ? 2 : 1;
-        this.updateItemQuality(item, increase);
-      }
-      if (item.name == BACKSTAGE_PASS) {
-        this.updateBackstagePassQuality(item);
-      }
+      this.updateItemQuality(item);
       if (hasSellByDate) {
         item.sellIn = item.sellIn - 1;
       }
@@ -38,19 +25,37 @@ export class GildedRose {
     return this.items;
   }
 
-  private updateItemQuality(item: Item, qualityOffset: number) {
+  private updateItemQuality(item: Item): void {
+    const doesDecrease = item.name !== AGED_BRIE && item.name !== BACKSTAGE_PASS && item.name !== SULFURAS;
+    const isExpired = item.sellIn < 1;
+
+    if (doesDecrease) {
+      const decreaseRate = this.getDecreaseRate(item);
+      const decrease = isExpired ? 2 * decreaseRate : decreaseRate;
+      this.adjustItemQuality(item, decrease);
+    }
+    if (item.name === AGED_BRIE) {
+      const increase = isExpired ? 2 : 1;
+      this.adjustItemQuality(item, increase);
+    }
+    if (item.name == BACKSTAGE_PASS) {
+      this.updateBackstagePassQuality(item);
+    }
+  }
+
+  private adjustItemQuality(item: Item, qualityOffset: number) {
     const updatedQuality = item.quality + qualityOffset;
     item.quality = this.roundQuality(updatedQuality);
   }
 
   private updateBackstagePassQuality(item: Item) {
-    this.updateItemQuality(item, 1);
+    this.adjustItemQuality(item, 1);
 
     if (item.sellIn < 11) {
-      this.updateItemQuality(item, 1);
+      this.adjustItemQuality(item, 1);
     }
     if (item.sellIn < 6) {
-      this.updateItemQuality(item, 1);
+      this.adjustItemQuality(item, 1);
     }
     if (item.sellIn < 1) {
       this.resetQuality(item);
